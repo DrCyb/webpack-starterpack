e48e794e9eb9d7f3d3923bebd81f0f42b4051f1b
StephanKritzler <stephan.kritzler@gmail.com>
2018-05-28 14:46:06 +0200

    several changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,25 @@
 const path = require("path");
+const webpack = require('webpack');
 
 module.exports = {
     // The main file for our JS
     entry: "./src/index.js",
+    
     // The output files
     output: {
         // Where we want to put our  output files
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "/dist/assets"),
 
         //The name for our files
         filename: "bundle.js"
     },
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         rules: [{
             test: /\.css$/,
@@ -20,4 +29,7 @@ module.exports = {
             ]
         }]
     }
+
 };
+
+// yarn run build   or    yarn run start
